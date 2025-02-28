14f5ee1bbd7205c366fba0ff1cc1f14d6f87990c
bgaenzle <bagaenzl@ncsu.edu>
2025-02-28 11:30:24 -0500

    iterating through groups loop

--- Post_Processing_Scripts/Specific_Analysis_Scripts/BOEM.py
+++ Post_Processing_Scripts/Specific_Analysis_Scripts/BOEM.py
@@ -4,6 +4,7 @@ import datetime as dt
 import pandas as pd
 import os 
 import time
+import process_Sig1k as ps
 
 from Post_Processing_Scripts.process_Sig1k import dtnum_dttime_adcp, read_raw_h5, remove_low_correlations, \
     transform_beam_ENUD, save_data
@@ -72,6 +73,32 @@ def average_vel_plots(path):
 # endtime = time.time()
 #
 # print("Time taken was", start_time - endtime, "seconds")
+
+# This is how I was running the process code before this doc, if this helps
+
+# directory_path = r"/Volumes/kanarde/BOEM/deployment_1/Raw/S0_103080_hdf/"
+# save_dir = r"/Volumes/kanarde/BOEM/deployment_1/Processed/S0_103080/"
+
+
+# files=os.listdir(directory_path)
+# print(files)
+# i = 0 # need to change if starting with group 1
+# for file_name in files:
+#      i+=1
+#      print(f"Processing Group {i}")
+#      path = os.path.join(directory_path, file_name)
+#      print(path)
+#      save_path_name = os.path.join(save_dir, f"Group{i}")
+#      #os.makedirs(os.path.dirname(save_path_name), exist_ok=True) # create a new group folder if not already present
+#      Data=ps.read_raw_h5(path)
+#      print(f"read in data")
+#      print(Data["CellDepth"])
+#      Data=ps.remove_low_correlations(Data)
+#      print(f"removed low corr")
+#      Data=ps.transform_beam_ENUD(Data)
+#      print("transformed to ENUD")
+#      ps.save_data(Data, save_dir)
+#      print(f"Processed {file_name} and saved to {save_dir}")
 
 #average_vel_plots(path)
 
