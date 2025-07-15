86dbe45a882c25a691ed892809395c019c4dbf15
Lwalkl <lwlavengood@gmail.com>
2025-07-15 10:24:17 -0400

    Bug fix to make sure sedtime is saved correctly

--- Post_Processing_Scripts/OLD/analysis_bulkstats.py
+++ Post_Processing_Scripts/OLD/analysis_bulkstats.py
@@ -719,6 +719,7 @@ def bulk_stats_analysis(
     Waves["Echo1avg"].to_hdf(os.path.join(save_dir, "Echo1avg"), key="df", mode="w")
     Waves["Echo2avg"].to_hdf(os.path.join(save_dir, "Echo2avg"), key="df", mode="w")
     Waves["vertavg"].to_hdf(os.path.join(save_dir, "Echo2avg"), key="df", mode="w")
+    Waves["sedtime"].to_hdf(os.path.join(save_dir, "SedTime"), key="df", mode="w")
 
     endtime = time.time()
 
