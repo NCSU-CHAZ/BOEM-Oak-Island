395760a8ff1ef87abc40df2f622b88f9a555e20e
Lwalkl <lwlavengood@gmail.com>
2025-08-15 17:16:30 -0400

    Edits to pytest to make them more clear

--- tests/test_sediment.py
+++ tests/test_sediment.py
@@ -1,35 +1,47 @@
 import numpy as np
 import pandas as pd
 import datetime as dt
-from scipy.signal import detrend
+import scipy.signal as sig
 
 # Import your functions here
-from Post_Processing_Scripts.sediment.spectral_sediment import welch_method, waveNumber_dispersion, check_psd_variance, despiker
+from Post_Processing_Scripts.sediment.spectral_sediment import welch_method, waveNumber_dispersion, despiker
 
 def test_variance_preservation_white_noise():
-    # White noise signal
+    # White noise signal 
     np.random.seed(0)
-    fs = 10  # Hz
-    N = 1024
-    noise = np.random.randn(N, 1)
-
+    fs = 2  # Hz
+    N = 10240
+    Nens = 1024
+    t = np.arange(N) / fs
+    f0 = 5  # Hz
+    noise = np.sin(2*np.pi*f0*t)[:, None] + np.random.randn(N, 1)
+    
     # Run Welch
-    M = 4
     overlap = 0.5
-    psd, freq = welch_method(noise, 1/fs, M, overlap)
 
     # Check variance match
-    ratio = check_psd_variance(noise, fs, M, overlap, welch_method)
+    dt = 1 / fs
+
+    psd, freq = welch_method(noise, dt, Nens, overlap)
+    df = freq[1] - freq[0]
+
+    var_time = np.var(noise, axis = 0)
+    var_spec = np.sum(psd, axis=0) * df
+
+    # return ratio and optionally whether within tolerance
+    ratio = var_spec / var_time
+    
     assert np.allclose(ratio, 1.0, rtol=0.1), f"Variance ratio {ratio} out of tolerance"
 
 def test_frequency_detection_sine_wave():
     fs = 100  # Hz
     N = 4096
+    Nens = 1024
     t = np.arange(N) / fs
     f0 = 5  # Hz
     sine_wave = np.sin(2*np.pi*f0*t)[:, None]
 
-    psd, freq = welch_method(sine_wave, 1/fs, M=8, overlap=0.5)
+    psd, freq = welch_method(sine_wave, 1/fs, Nens, overlap=0.5)
     peak_freq = freq[np.argmax(psd[:, 0])]
     assert abs(peak_freq - f0) < 0.5, f"Peak frequency {peak_freq} not near {f0}"
 
@@ -52,4 +64,4 @@ def test_despiker_removes_spike():
 
     cleaned, mask = despiker(ssc, fs=fs)
     assert mask[50], "Spike not detected"
-    assert abs(cleaned.iloc[50] - cleaned.iloc[49]) < 10, "Spike not corrected properly"
+    assert abs(cleaned.iloc[50].values - cleaned.iloc[49].values) < 10, "Spike not corrected properly"
